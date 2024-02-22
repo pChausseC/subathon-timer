1c91d9009d4a5f136113540e3994f8d8568b70aa
pabloCC <chaussepablo@gmail.com>
2024-02-21 21:38:33 -0500

    Add goal section

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Goal } from "@/components/goal";
 import { ProgressBar } from "@/components/progress-bar";
 import { SocketIndicator } from "@/components/socket-indicator";
 import { TimeElapsed } from "@/components/time-elapsed";
@@ -5,15 +6,16 @@ import { TimeLeft } from "@/components/time-left";
 import { Toaster } from "@/components/ui/toaster";
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center e">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="relative w-full">
         <Toaster />
       </div>
-      <div className="z-10 flex flex-col items-center justify-center gap-2 bg-black rounded-lg border-2 border-white p-5">
+      <div className="z-10 flex flex-col items-center justify-center gap-2 rounded-[10px] border-2 border-white bg-black px-5 pb-8 pt-4 relative">
         <SocketIndicator />
         <TimeLeft />
         <TimeElapsed />
         <ProgressBar />
+        <Goal />
       </div>
     </main>
   );
---
+++ web/src/components/goal.tsx
@@ -0,0 +1,5 @@
+export const Goal = () => {
+  return (
+    <span contentEditable className="absolute top-[calc(100%-20px)] min-w-[40%] max-w-[80%] rounded-lg bg-foreground px-5 py-1 text-center text-2xl font-bold uppercase text-background" />
+  );
+};
