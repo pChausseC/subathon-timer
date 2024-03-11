31f17c9767dbe8c160516b5c3ea6e213fa66ac8c
pabloCC <chaussepablo@gmail.com>
2024-03-11 00:01:00 -0400

    max goal step 500

--- web/src/components/progress-bar.tsx
+++ web/src/components/progress-bar.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useSocket } from "@/providers/socket-provider";
 import { Progress } from "./ui/progress";
 function getNextMultipleOf500(x: number): number {
-  return Math.ceil(x / 500) * 500;
+  return Math.ceil((x || 1) / 500) * 500;
 }
 export const ProgressBar = () => {
   const [progress, setProgress] = useState(0);
@@ -25,7 +25,7 @@ export const ProgressBar = () => {
   }, [socket]);
   return (
     <Progress value={progress}>
-      <span className="relative bottom-[8px] px-1 font-wide text-xs font-[500] leading-none invert tracking-widest">
+      <span className="relative bottom-[8px] px-1 font-wide text-xs font-[500] leading-none tracking-widest invert">
         {Math.round(total)}/{getNextMultipleOf500(total)}
       </span>
     </Progress>
